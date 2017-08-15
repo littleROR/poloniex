975ba560fcdf8d55ab50b9cc5b26291d36d98ee4
Bogdan <bogdan.mihai.timofte@gmail.com>
2017-08-15 13:06:58 +0300

    Added to_hash method for history objects

--- lib/poloniex/history.rb
+++ lib/poloniex/history.rb
@@ -1,9 +1,17 @@
 module Poloniex
   class History
     attr_reader :date, :high, :low, :open, :close, :volume, :quote_volume, :weighted_average
-      def []=(*options)
+    
+    def []=(*options)
       key, value = normalize(*options)
       self.instance_variable_set("@#{key.to_underscore}", value)
+    end
+
+    def to_hash
+      instance_variables.inject(Hash.new) do |result, var|
+          result[var.to_s.gsub("@", "").to_sym] = instance_variable_get(var)
+          result
+      end
     end
 
     private
